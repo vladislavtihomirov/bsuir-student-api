4d76855c3b202208383cc9d55e899ab5d0ba4b11
Vlad Tihomirov <vladtihomirov010203@gmail.com>
2022-09-03 16:10:27 +0300

    - pong

--- api/api.py
+++ api/api.py
@@ -20,7 +20,7 @@ api_router = APIRouter(default_response_class=StatusResponse)
 
 @api_router.get('/ping')
 def pong():
-    return pong
+    return 'pong'
 
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
